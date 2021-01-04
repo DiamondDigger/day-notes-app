10f62850dd4804f1477426b8255d85bdd27d7272
naglec13 <naglec13@gmail.com>
2021-01-04 23:46:49 +0300

    add more styles to the Calendar component

--- src/features/calendar/Calendar.tsx
+++ src/features/calendar/Calendar.tsx
@@ -1,18 +1,50 @@
 import React from 'react'
 import styles from './Calendar.module.css'
 
-const days = [1,2,3,4,5]
+const week = [1,2,3,4,5,6,7]
 
 const Calendar = () => {
     return (
-        <div className={styles.root}>
-            {days.map((day) => (
-                <div key={day} className={styles.dayCell}>
-                    {day}
+        <>
+            <div className={styles.container}>
+                <div className={styles.root}>
+                    {
+                    week.map((day) => (
+                        <div key={day} className={styles.dayCell}>
+                            {day}
+                        </div>
+                        ))
+                    }
                 </div>
-            ))}
-        </div>
+                <div className={styles.root}>
+                    {
+                    week.map((day) => (
+                        <div key={day} className={styles.dayCell}>
+                            {day}
+                        </div>
+                        ))
+                    }
+                </div>
+                <div className={styles.root}>
+                    {
+                    week.map((day) => (
+                        <div key={day} className={styles.dayCell}>
+                            {day}
+                        </div>
+                        ))
+                    }
+                </div>
+                <div className={styles.root}>
+                    {
+                    week.map((day) => (
+                        <div key={day} className={styles.dayCell}>
+                            {day}
+                        </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </>
     )
 }
-
 export default Calendar
